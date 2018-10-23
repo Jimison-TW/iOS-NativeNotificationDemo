1bda9cc46b05a063e1e906adbe6d4468f0ec1533
Jimison <jimison@jimisonde-macbook-pro.local>
2018-10-23 15:48:19 +0800

    Initial Commit

---
+++ NativeNotificationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NativeNotificationTest
+//
+//  Created by Jimison on 2018/10/23.
+//  Copyright © 2018年 Jimison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
